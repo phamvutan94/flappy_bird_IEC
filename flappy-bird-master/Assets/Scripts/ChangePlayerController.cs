e519ff472252b9a997da87412eca71871fd649f2
phamvutan94 <c2101lm.pvtan@gmail.com>
2022-08-06 09:35:02 +0700

    - thêm Prefab Player - thêm script ChangePlayerController để xử lý đổi màu và animation player - thêm animator Player

---
+++ flappy-bird-master/Assets/Scripts/ChangePlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangePlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private SpriteRenderer flapp_bird;
+
+    [SerializeField]
+    private Sprite[] flapp_bird_Image;
+
+    [SerializeField]
+    private Animator Anim;
+
+    private void Awake() {
+        Random_bird();
+     }
+
+    void Start() { }
+
+    // Update is called once per frame
+    void Update() { }
+
+    private void Random_bird()
+    {
+        // tạo số ngẫu nhiên trong khoảng 0 - số lượng Sprite của player
+        int a = Random.Range(0, flapp_bird_Image.Length);
+       //set Sprite cho player
+        flapp_bird.sprite = flapp_bird_Image[a];
+        // set animation cho player;
+        Anim.SetInteger("Player", a);
+    }
+}
